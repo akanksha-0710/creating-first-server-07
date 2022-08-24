0fb46e39a3f9e4e8c1cfe85c7474856aebedb6fe
akanksha-0710 <akankshajadhav0710@gmail.com>
2022-08-24 14:15:08 +0530

    Creating our first server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const http = require('http');
+const http = require("http");
 
-const port = 8091;
+const port = 8091;  //local port no
 
 http
     .createServer((request, response) => {
+          //callback function
         response.writeHead(200, {"content-type":"text / html"});
         response.write("<h1>hello,this is from my server</h1>");
         response.end();
     })
-        .listen(port, () => {
+        .listen(port, () => {          
+             //callback function
             console.log(`Nodejs server started on port ${port}`);
         });
 
