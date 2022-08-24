082e9470f4f65e92ccdeb55bd48d21b990172822
akanksha-0710 <akankshajadhav0710@gmail.com>
2022-08-24 18:29:45 +0530

    first server using http methods

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const http = require("http");
 
-const port = 8091;  //local port no
+
+const port = 8091;  
+
+const toDoList = ["Completed Node Byte", "Play cricket"];
 
 http
-    .createServer((request, response) => {
-          //callback function
-        response.writeHead(200, {"content-type":"text / html"});
-        response.write("<h1>hello,this is from my server</h1>");
-        response.end();
+    .createServer((req, res) => {
+        
+        const { method, url } = req;
+        if (url === "/todos"){
+            if (method ==="GET"){
+                res.writeHead(200);
+                res.write(toDoList.toString());
+            }else if (method === "POST"){
+                let body ="";
+                req.on('error',(err) =>{
+                    console.error(err);   
+                })
+                .on('data',(chunk)=> {
+                    body +=chunk;
+                    console.log(chunk);                
+                })
+                .on('end',()=>{
+                    body = JSON.parse(body);
+                    console.log("data:",body);
+                });
+            }
+            else{
+                res.writeHead(501);
+            }
+        } else{
+            res.writeHead(404);
+        }
+        res.end();
     })
-        .listen(port, () => {          
-             //callback function
-            console.log(`Nodejs server started on port ${port}`);
-        });
+    .listen(port, () => {
+       
+        console.log(`Nodejs server started on port ${port}`);
+    });
 
 // http://Localhost:8091
