f9989794a244f77db728b9e6edc3108485478753
akanksha-0710 <akankshajadhav0710@gmail.com>
2022-08-27 15:37:02 +0530

    create  1st server using express.js

--- server.js
+++ server.js
@@ -1,43 +1,60 @@
-const http = require("http");
+const express = require('express')
 
+//initilization
+const app = express();
+//application now will use json format for data
+app.use(express.json());
 
 const port = 8091;  
 
 const toDoList = ["Completed Node Byte", "Play cricket"];
 
-http
-    .createServer((req, res) => {
-        
-        const { method, url } = req;
-        if (url === "/todos"){
-            if (method ==="GET"){
-                res.writeHead(200);
-                res.write(toDoList.toString());
-            }else if (method === "POST"){
-                let body ="";
-                req.on('error',(err) =>{
-                    console.error(err);   
-                })
-                .on('data',(chunk)=> {
-                    body +=chunk;
-                    console.log(chunk);                
-                })
-                .on('end',()=>{
-                    body = JSON.parse(body);
-                    console.log("data:",body);
-                });
-            }
-            else{
-                res.writeHead(501);
-            }
-        } else{
-            res.writeHead(404);
+//http://Localhost:8091/todos
+app.get("/todos",(req,res)=>{
+    res.status(200).send(toDoList);
+});
+
+app.post("/todos",(req,res)=>{
+   let newToDoItem=req.body.item; 
+   toDoList.push(newToDoItem);
+   res.status(201).send({
+     message:"Task added successfully",
+   });
+});
+
+app.delete("/todos",(req,res)=>{
+    const itemToDelete = req.body.item;
+
+    toDoList.find((element,index)=>{
+        if(element===itemToDelete){
+            toDoList.splice(index,1);
         }
-        res.end();
-    })
-    .listen(port, () => {
-       
-        console.log(`Nodejs server started on port ${port}`);
     });
 
-// http://Localhost:8091
+    res.status(202).send({
+        message:`Deleted item - ${req.body.item}`,
+    });
+
+});
+
+
+//change to another routers(just some additional eg)
+//app.get("/todos/creat",)
+
+// all the other methods on particular routers
+app.all("/todos",(req,res)=>{
+    res.status(501).send();
+});   
+
+//all the other routers
+app.all('*',(req,res)=>{
+     res.status(404).send();
+});
+
+
+
+
+
+app.listen(port,() =>{
+ console.log(`Nodejs server started on port ${port}`);
+});
